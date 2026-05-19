5be11463687187e0ca70de092ec8edb98aa3bbcd
ANH KHOA <anhkhoa512005@gmail.com>
2026-05-19 10:55:32 +0700

    fix 3 dau hieu

--- src/pages/Article7Page.tsx
+++ src/pages/Article7Page.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import PublicNavbar from "@/components/PublicNavbar";
 import PublicFooter from "@/components/PublicFooter";
 import { Helmet } from 'react-helmet-async';
